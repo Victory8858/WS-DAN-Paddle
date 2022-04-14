887c4291d3cf20057c538bf1e8b22dca1000311d
‘gaoding3070laptop’ <gd.1999@163.com>
2022-04-14 22:17:12 +0800

    update code

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ from models.wsdan import WSDAN
 from utils import CenterLoss, AverageMeter, TopKAccuracyMetric, batch_augment
 
 # which dataset you want to test
-config.target_dataset = 'car'  # it can be 'car', 'bird', 'aircraft'
+config.target_dataset = 'aircraft'  # it can be 'car', 'bird', 'aircraft'
 
 # logging config
 logging.basicConfig(
@@ -52,7 +52,7 @@ feature_center = paddle.zeros(shape=[num_classes, config.num_attentions * net.nu
 if config.target_dataset == 'bird':
     net_state_dict = paddle.load("FGVC/bird/ckpt/bird_model89.23.pdparams")  
 if config.target_dataset == 'aircraft':
-    net_state_dict = paddle.load("FGVC/aircraft/ckpt/aircraft_model93.04.pdparams") 
+    net_state_dict = paddle.load("FGVC/aircraft/ckpt/aircraft_model94.03.pdparams")
 if config.target_dataset == 'car':
     net_state_dict = paddle.load("FGVC/car/ckpt/car_model94.88.pdparams")
 net.set_state_dict(net_state_dict)
