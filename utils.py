49bd199dfa70387e086ad3d03a2efa593733ddc5
‘gaoding3070laptop’ <gd.1999@163.com>
2022-04-14 21:49:02 +0800

    update code

--- utils.py
+++ utils.py
@@ -63,7 +63,6 @@ class TopKAccuracyMetric(Metric):
 
         for i, k in enumerate(self.topk):
             temp = paddle.reshape(correct[:k], [-1])
-            # correct_k = correct[:k].reshape(-1).float().sum(0)
             correct_k = temp.sum(0)
             self.corrects[i] += correct_k.item()
 
@@ -80,61 +79,6 @@ class Callback(object):
 
     def on_epoch_end(self, *args):
         pass
-
-
-class ModelCheckpoint(Callback):
-    def __init__(self, savepath, monitor='val_topk_accuracy', mode='max'):
-        self.savepath = savepath
-        self.monitor = monitor
-        self.mode = mode
-        self.reset()
-        super(ModelCheckpoint, self).__init__()
-
-    def reset(self):
-        if self.mode == 'max':
-            self.best_score = float('-inf')
-        else:
-            self.best_score = float('inf')
-
-    def set_best_score(self, score):
-        if isinstance(score, np.ndarray):
-            self.best_score = score[0]
-        else:
-            self.best_score = score
-
-    def on_epoch_begin(self):
-        pass
-
-    def on_epoch_end(self, logs, net, **kwargs):
-        current_score = logs[self.monitor]
-        if isinstance(current_score, np.ndarray):
-            current_score = current_score[0]
-
-        if (self.mode == 'max' and current_score > self.best_score) or \
-                (self.mode == 'min' and current_score < self.best_score):
-            self.best_score = current_score
-
-            if isinstance(net, paddle.DataParallel):
-                state_dict = net.module.state_dict()
-            else:
-                state_dict = net.state_dict()
-
-            # for key in state_dict.keys():
-            #     state_dict[key] = state_dict[key].cpu()
-            #
-            if 'feature_center' in kwargs:
-                feature_center = kwargs['feature_center']
-                #     feature_center = feature_center.cpu()
-
-                paddle.save({
-                    'logs': logs,
-                    'state_dict': state_dict,
-                    'feature_center': feature_center}, self.savepath)
-
-            else:
-                paddle.save({
-                    'logs': logs,
-                    'state_dict': state_dict}, self.savepath)
 
 
 # augment function
