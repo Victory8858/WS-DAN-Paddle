de5ec7a036e48cb3bc147ad0a01aac0617a92c60
‘gaoding3070laptop’ <gd.1999@163.com>
2022-04-14 23:45:13 +0800

    update code

--- config.py
+++ config.py
@@ -4,10 +4,13 @@
 - @Description: 参数配置文件
 """
 
+# Choose Dataset
+target_dataset = 'aircraft'  # options: 'aircraft', 'bird', 'car'
+
 # Hyper Parameter
 workers = 0  # number of Dataloader workers Windows 不支持多进程
 epochs = 80  # number of epochs
-batch_size = 6  # batch size
+batch_size = 6  # batch size, if train, you should set to 12
 learning_rate = 0.001  # initial learning rate
 
 # Model
@@ -16,9 +19,6 @@ image_size = (512, 512)
 net_name = 'inception_mixed_6e'  # feature extractor
 num_attentions = 32  # number of attention maps
 beta = 5e-2  # param for update feature centers
-
-# Dataset Path Config
-target_dataset = 'aircraft'  # options: 'aircraft', 'bird', 'car'
 
 # Model and Logging Save Path
 save_dir = "C:/Users/Victory/Desktop/WS-DAN-Paddle-Victory8858/FGVC/" + target_dataset + "/ckpt/"  # Windows Path
